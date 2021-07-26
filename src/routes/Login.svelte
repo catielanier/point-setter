fe2ff5fd983bceff7d732959794469559cce5c8b
Corey Lanier <iam@coreylanier.com>
2021-07-26 18:55:49 -0400

    create router + login prompt

---
+++ src/routes/Login.svelte
@@ -0,0 +1,17 @@
+<script>
+  let email = "",
+    password = "",
+    loading = false,
+    success = false,
+    error;
+  const doLogin = async () => {
+    loading = true;
+    console.log(email, password, loading);
+  };
+</script>
+
+<form on:submit={doLogin}>
+  <input type="text" placeholder="Email" bind:value={email} />
+  <input type="password" placeholder="Password" bind:value={password} />
+  <button type="submit">Login</button>
+</form>

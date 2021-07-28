bbf07a12b45813de294bfd31d21cb5a6766b3360
Corey Lanier <iam@coreylanier.com>
2021-07-27 22:14:23 -0400

    retrieve teachers

--- src/components/Setter.vue
+++ src/components/Setter.vue
@@ -1,6 +1,25 @@
 <script>
+import axios from "axios";
 export default {
   name: "Setter",
+  data() {
+    return {
+      teachers: [],
+      courses: [],
+      teacher: null,
+      course: null,
+      assignments: [],
+    };
+  },
+  async mounted() {
+    const res = await axios.get("/api/teachers");
+    this.teachers = res.data.data;
+  },
+  methods: {
+    getCourses: async function () {},
+    getAssignments: async function () {},
+    submitPoints: async function () {},
+  },
 };
 </script>
 

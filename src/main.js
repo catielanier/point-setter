46b7417901ebdeb0d0150307a8e4b508f239bafb
Corey Lanier <iam@coreylanier.com>
2021-07-31 19:42:23 -0400

    add vue-select

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import vSelect from "vue-select";
+import App from "./App.vue";
 
-Vue.config.productionTip = false
+Vue.component("v-select", vSelect);
+
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");

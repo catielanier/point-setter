0f13fb78f2dc76259594ad5020bf46b7ec8bdbed
Corey Lanier <iam@coreylanier.com>
2021-07-31 19:15:34 -0400

    Frontend setting

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="main-container">
     <h1>Point Setter</h1>
     <Login v-if="!userId" :setId="setId" />
     <Setter v-else />
@@ -34,15 +34,15 @@ export default {
 
 <style>
 body {
-  background: #2c3e50;
+  background: wheat;
 }
-#app {
+.main-container {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   font-size: 1.6rem;
   text-align: center;
-  color: white;
+  color: black;
   margin-top: 60px;
 }
 </style>
--- src/components/Setter.vue
+++ src/components/Setter.vue
@@ -1,4 +1,19 @@
+<template>
+  <div class="setter-container">
+    <div class="dropdowns">
+      <v-select
+        class="select-styles"
+        label="fullName"
+        :options="teachers"
+        @input="getCourses"
+        placeholder="Select a teacher"
+      />
+    </div>
+  </div>
+</template>
+
 <script>
+import "vue-select/dist/vue-select.css";
 import axios from "axios";
 export default {
   name: "Setter",
@@ -9,20 +24,52 @@ export default {
       teacher: null,
       course: null,
       assignments: [],
+      curriculumTypes: [],
+      curriculum: null,
     };
   },
   async mounted() {
     const res = await axios.get("/api/teachers");
+    res.data.data.forEach((teacher) => {
+      teacher.fullName = `${teacher.lastName}, ${teacher.firstName} (${teacher.designation})`;
+    });
     this.teachers = res.data.data;
   },
   methods: {
-    getCourses: async function () {},
+    getCourses: async function (e) {
+      const { apiKey } = e;
+      this.teacher = apiKey;
+      try {
+        const res = await axios({
+          method: "GET",
+          url: "/api/courses",
+          params: {
+            apiKey,
+          },
+        });
+        const courses = [];
+        console.log(res);
+        res.data.courses.forEach((course) => {
+          course.fullName = `${course.name} (${course.term.name})`;
+          courses.push(course);
+        });
+        this.courses = courses;
+      } catch (e) {
+        console.log(e);
+      }
+    },
     getAssignments: async function () {},
     submitPoints: async function () {},
   },
 };
 </script>
 
-<template>
-  <h1>Setter</h1>
-</template>
+<style>
+.dropdowns {
+  max-width: 640px;
+  margin: 0 auto;
+}
+.select-styles {
+  font-size: 1.2rem;
+}
+</style>

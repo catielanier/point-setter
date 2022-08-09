bd033345fb285ac9c6a53e1abda099a002bd6e46
Corey Lanier <iam@coreylanier.com>
2022-08-09 19:47:02 -0400

    fix course endpoint

--- api/_routes/courseRoutes.js
+++ api/_routes/courseRoutes.js
@@ -1,25 +1,58 @@
 const express = require("express");
+const axios = require('axios')
 
-const { setParams, apiPagination } = require("../_helpers/pagination");
-
-const { API_KEY: access_token } = require("../_utils/constants");
+const { API_KEY: access_token, ACCOUNT_NO: accountNo } = require("../_utils/constants");
 
 const router = express.Router();
 
 router.route("/").get(async (req, res) => {
   try {
-    const state = ["available", "unpublished"];
-    const include = ["term"];
-    const url = "https://canvas.instructure.com/api/v1/courses";
-
-    const params = setParams(access_token, state, include);
-    const courses = await apiPagination(url, params, []);
-    res.status(200).json({
-      courses,
-    });
-  } catch (e) {
-    res.status(400);
-  }
+    const courses = [];
+		const result = await axios({
+      method: "GET",
+			url: `https://vpa.instructure.com/api/v1/accounts/${accountNo}/courses`,
+			headers: {
+        Accept: "application/json+canvas-string-ids"
+			},
+			params: {
+        access_token,
+				per_page: 100
+			}
+		});
+		result.data.forEach(course => {
+			courses.push(course);
+		});
+		if (result.data.length === 100) {
+			let currentPage = 2;
+			async function apiPagination() {
+				const courseRes = await axios({
+					method: "GET",
+					url: `https://vpa.instructure.com/api/v1/accounts/${accountNo}/courses`,
+					headers: {
+						Accept: "application/json+canvas-string-ids"
+					},
+					params: {
+						access_token,
+						per_page: 100,
+						page: currentPage
+					}
+				});
+				courseRes.data.forEach(course => {
+					courses.push(course);
+				});
+				if (courseRes.data.length === 100) {
+					currentPage += 1;
+					await apiPagination();
+				}
+			}
+			await apiPagination();
+		}
+		res.status(200).json({
+			data: courses
+		});
+	} catch (e) {
+		res.status(400);
+	}
 });
 
 exports.router = router;
